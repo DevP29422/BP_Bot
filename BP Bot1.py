a4a0f31ef30eb21f298ddb9f2854f79b2103b40e
BrickPlanetSpyro <40009512+BrickPlanetSpyro@users.noreply.github.com>
2018-06-24 12:16:07 -0400

    Update BP Bot1.py
    Added a try except block, requests module can fail so It's important to catch the error

--- BP Bot1.py
+++ BP Bot1.py
@@ -14,30 +14,31 @@ async def on_ready():
     print('------')
 @bot.command(pass_context=True)
 async def whois(ctx, user):
-    r = requests.get("https://www.brickplanet.com/web-api/users/get-user/"+user)
-    data = r.json()
-    u_name = data["Username"]
-    u_ID = data["ID"]
-    u_fp = data["ForumPosts"]
-    thumb = data["AvatarImage"]
-    u_status = data["Status"]
-    u_about = data["About"]
-    u_frds = data["NumFriends"]
-    u_groups = data["NumGroups"]
-    u_net = data["NetWorth"]
-    #############################################################################
+    try:
+        data = requests.get("https://www.brickplanet.com/web-api/users/get-user/"+user).json()
+        u_name = data["Username"]
+        u_ID = data["ID"]
+        u_fp = data["ForumPosts"]
+        thumb = data["AvatarImage"]
+        u_status = data["Status"]
+        u_about = data["About"]
+        u_frds = data["NumFriends"]
+        u_groups = data["NumGroups"]
+        u_net = data["NetWorth"]
+        #############################################################################
         embed = discord.Embed(title="{}'s Information".format(user), description = "This is all I can find!", color=0x00ff00)
-    embed.add_field(name="Username:", value=u_name, inline=True)
-    embed.add_field(name="ID:", value=u_ID, inline=True)
-    embed.add_field(name="Forum Posts:", value=u_fp, inline=True)
-    embed.add_field(name="Current Status:", value=u_status, inline=True)
-    embed.add_field(name="About:", value=u_about, inline=True)
-    embed.add_field(name="Friends:", value=u_frds, inline=True)    
-    embed.add_field(name="In Groups:", value=u_groups, inline=True)  
-    embed.add_field(name="Networth:", value=u_net, inline=True) 
-    embed.set_thumbnail(url="https://cdn.brickplanet.com/"+thumb+".png")
-    await bot.say(embed=embed)
-
+        embed.add_field(name="Username:", value=u_name, inline=True)
+        embed.add_field(name="ID:", value=u_ID, inline=True)
+        embed.add_field(name="Forum Posts:", value=u_fp, inline=True)
+        embed.add_field(name="Current Status:", value=u_status, inline=True)
+        embed.add_field(name="About:", value=u_about, inline=True)
+        embed.add_field(name="Friends:", value=u_frds, inline=True)    
+        embed.add_field(name="In Groups:", value=u_groups, inline=True)  
+        embed.add_field(name="Networth:", value=u_net, inline=True) 
+        embed.set_thumbnail(url="https://cdn.brickplanet.com/"+thumb+".png")
+        await bot.say(embed=embed)
+    except:
+        bot.say('An error occurred')
     
 
 bot.run(os.getenv('TOKEN'))
